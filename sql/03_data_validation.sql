e7de53b80ba578158dd4e9ba194fdbaa712e3146
Mariana Barboza <59181543+marianafbarboza@users.noreply.github.com>
2026-04-09 18:40:25 -0300

    Update 03_data_validation.sql

--- sql/03_data_validation.sql
+++ sql/03_data_validation.sql
@@ -1,1 +1,18 @@
+-- Duplicates check
+SELECT
+  order_id,
+  customer_id,
+  product_id,
+  order_date,
+  COUNT(*) AS total
+FROM `projetoetl-466019.dados.superstore`
+GROUP BY order_id, customer_id, product_id, order_date
+HAVING COUNT(*) > 1;
 
+-- Basic statics
+SELECT
+  MIN(sales) AS min_sales,
+  MAX(sales) AS max_sales,
+  AVG(sales) AS avg_sales,
+  STDDEV(sales) AS std_sales
+FROM `projetoetl-466019.dados.superstore`;
