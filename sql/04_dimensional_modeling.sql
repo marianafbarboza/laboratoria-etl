d3381fc5d0405c763d52a0fc2f5a8e07627f5321
Mariana Barboza <59181543+marianafbarboza@users.noreply.github.com>
2026-04-09 18:41:25 -0300

    Update 04_dimensional_modeling.sql

--- sql/04_dimensional_modeling.sql
+++ sql/04_dimensional_modeling.sql
@@ -1,1 +1,11 @@
-
+-- Customer dimension
+CREATE OR REPLACE TABLE `projetoetl-466019.dados.dim_cliente` AS
+SELECT DISTINCT
+  customer_id,
+  customer_name,
+  segment,
+  city,
+  state,
+  region,
+  country
+FROM `projetoetl-466019.dados.superstore`;
