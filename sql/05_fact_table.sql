0f9e7cff0050ff0bcee322ca4f18703a0add2c31
Mariana Barboza <59181543+marianafbarboza@users.noreply.github.com>
2026-04-09 18:42:18 -0300

    Update 05_fact_table.sql

--- sql/05_fact_table.sql
+++ sql/05_fact_table.sql
@@ -1,1 +1,18 @@
-
+CREATE OR REPLACE TABLE `projetoetl-466019.dados.fato_pedidos` AS
+SELECT
+  row_id,
+  order_id,
+  customer_id,
+  product_id,
+  order_date AS order_date_id,
+  ship_date AS ship_date_id,
+  quantity,
+  discount,
+  sales,
+  profit,
+  shipping_cost,
+  ship_mode,
+  order_priority,
+  market,
+  market2
+FROM `projetoetl-466019.dados.superstore`;
