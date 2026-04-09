1057a7644bd662395c4f9186a9e715c1e4f4f827
Mariana Barboza <59181543+marianafbarboza@users.noreply.github.com>
2026-04-09 18:37:52 -0300

    Update 01_data_quality_checks.sql

--- sql/01_data_quality_checks.sql
+++ sql/01_data_quality_checks.sql
@@ -1,1 +1,12 @@
-
+-- Verify null values dinamically
+SELECT
+  CONCAT(
+    'SELECT ',
+    STRING_AGG(
+      FORMAT("COUNTIF(%s IS NULL) AS nulos_%s", column_name, column_name),
+      ', '
+    ),
+    ' FROM projetoetl-466019.dados.superstore'
+  ) AS query
+FROM projetoetl-466019.dados.INFORMATION_SCHEMA.COLUMNS
+WHERE table_name = 'superstore';
