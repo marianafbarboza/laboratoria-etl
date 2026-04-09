6c97f47f61765d4a7dc03668dbe75ea839531b4a
Mariana Barboza <59181543+marianafbarboza@users.noreply.github.com>
2026-04-09 18:42:58 -0300

    Update 06_analytical_queries.sql

--- sql/06_analytical_queries.sql
+++ sql/06_analytical_queries.sql
@@ -1,1 +1,8 @@
-
+-- Total orders per year
+SELECT 
+  t.year AS order_year,
+  COUNT(*) AS total_orders
+FROM fato_pedidos f
+JOIN dim_tempo t 
+  ON f.order_date_id = t.date_id
+GROUP BY order_year;
