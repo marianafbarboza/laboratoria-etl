de3a40d39743612ef3b0394305227120cbdac250
Mariana Barboza <59181543+marianafbarboza@users.noreply.github.com>
2026-04-09 18:38:36 -0300

    Update 02_data_cleaning.sql

--- sql/02_data_cleaning.sql
+++ sql/02_data_cleaning.sql
@@ -1,1 +1,7 @@
-
+-- Example of text standardization
+SELECT
+  segment,
+  LOWER(TRIM(segment)) AS segment_padrao,
+  COUNT(*) AS total
+FROM `projetoetl-466019.dados.superstore`
+GROUP BY segment, segment_padrao;
